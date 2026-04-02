c8ba4edff67004e004c64b0de098742d803b6f60
Richie <d00280247@student.dkit.ie>
2026-04-02 01:53:00 +0100

    sword website skeleton

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/home');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
 });
